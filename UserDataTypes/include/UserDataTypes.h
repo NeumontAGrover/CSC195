e950ee5dda648cc51b1a35886251a73277a7dbaf
NeumontAGrover <agrover@student.neumont.edu>
2025-05-07 17:23:54 -0600

    Employee Payroll

---
+++ UserDataTypes/include/UserDataTypes.h
@@ -0,0 +1,16 @@
+#ifndef USERDATATYPES_H
+#define USERDATATYPES_H
+
+/* This generated file contains includes for project dependencies */
+#include "UserDataTypes/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
