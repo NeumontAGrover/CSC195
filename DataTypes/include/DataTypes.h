ea1705a110b14c63e67751a1bb584801a44c7ea1
NeumontAGrover <agrover@student.neumont.edu>
2025-05-07 09:29:03 -0600

    CSC195 DataTypes

---
+++ DataTypes/include/DataTypes.h
@@ -0,0 +1,16 @@
+#ifndef DATATYPES_H
+#define DATATYPES_H
+
+/* This generated file contains includes for project dependencies */
+#include "DataTypes/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
